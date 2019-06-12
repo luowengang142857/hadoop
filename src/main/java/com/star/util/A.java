25aa966953a759579fa3c8f9ffe9a5344cd3ce3c
lwg <15983316702@163.com>
2019-06-12 20:52:35 +0800

    新增了B类，修改了A类

--- src/main/java/com/star/util/A.java
+++ src/main/java/com/star/util/A.java
@@ -6,6 +6,9 @@ package com.star.util;
  */
 public class A {
     public static void main(String[] args) {
+        /**
+         * 你知道吗，这是我拉取下来的代码，并且修改了其中的类
+         */
         final A a = new A();
         a.print();
         new Thread(new Runnable() {
---
+++ src/main/java/com/star/util/B.java
@@ -0,0 +1,15 @@
+package com.star.util;
+
+/**
+ * @author lwg
+ * @Date: 2019/6/12 20:50
+ */
+public class B {
+    /**
+     * 新建立的类
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("hellp");
+    }
+}
